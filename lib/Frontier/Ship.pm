1c015cb1fba8c8008e2c446aefb60a5acd6cc564
jgoodman@bluehost.com <jgoodman@bluehost.com>
2015-03-27 21:33:02 -0600

    Added constructor method to init stats. Added stats method to return cached hashref. Added sub hull, sub shields, and sub energy. Added POD

--- lib/Frontier/Ship.pm
+++ lib/Frontier/Ship.pm
@@ -4,7 +4,31 @@ use strict;
 use warnings;
 use base 'Frontier::Base';
 
-sub stats { }
+sub new {
+    my $self = shift->SUPER::new(@_);
+    $self->$_ foreach (qw(hull shields energy));
+    return $self;
+}
+
+sub stats { return shift->{'stats'} //= { } }
+
+sub hull { return shift->_get_or_set('hull', @_) }
+
+sub shields { return shift->_get_or_set('shields', @_) }
+
+sub energy { return shift->_get_or_set('energy', @_) }
+
+sub _get_or_set {
+    my $self  = shift;
+    my $key   = shift;
+    my $value = shift;
+    my $stats = $self->stats;
+    if(defined $value) {
+        die "$key does not match type UINT" unless $value =~ m/^\d+$/;
+        $stats->{$key} = $value;
+    }
+    return $stats->{$key} //= 1;
+}
 
 1;
 
@@ -20,8 +44,24 @@ This module abstracts a ship
 
 =head1 METHODS
 
+=head2 new
+
+Constructor method which initializes stats for the ship object.
+
 =head2 stats
 
-Returns a hashref pertaining to the objects stats
+Returns a hashref pertaining to the objects stats.
+
+=head hull
+
+Get/set the INT value of "hull" which is a stat of a ship object.
+
+=head2 shields
+
+Get/set the  INT value of "shields" which is a stat of a ship object.
+
+=head2 energy
+
+Get/set the INT value of "energy" which is a stat of a ship object.
 
 =cut
